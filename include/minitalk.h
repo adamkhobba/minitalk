2950ae63ea4a6d6e679443e0f8404de1ab763408
adamkhobba <adamekhobba6@gmail.com>
2024-02-14 17:02:45 +0100

    add libft + add printf

---
+++ include/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adamkhobba <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 13:51:46 by adamkhobba        #+#    #+#             */
+/*   Updated: 2024/02/14 13:51:47 by adamkhobba       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include "../lib/Printf/ft_printf.h"
+#include "../lib/libft/libft.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
---
+++ src/server/server.c
@@ -0,0 +1,7 @@
+#include "../../include/minitalk.h"
+
+int main() {
+  ft_printf();
+
+  return 0;
+}
